0b5b49cad9668f10ca6796009aa0c27a3c0a8c0b
sytpb <songyantao@foxmail.com>
2022-07-08 16:36:57 +0800

    not allow to access by invaild path

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const stateRouter = require('./routes/state');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
